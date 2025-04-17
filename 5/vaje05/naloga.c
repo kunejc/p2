b3ed3ae273436b411ab2ab060e3a12c8706c0c32
kunejc <nejc.kunej@gmail.com>
2025-04-17 12:03:30 +0200

    vaje5

---
+++ 5/vaje05/naloga.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "naloga.h"
+
+int vsota(int* zac, int* kon) {
+    int sum = 0;
+    while (zac!=kon) {
+        sum += *zac;
+        zac++;
+    }
+    sum += *zac;
+    
+
+    return sum;
+}
+
+void indeksInKazalec(int* t, int* indeks, int** kazalec) {
+    if(*indeks == -1) *indeks = *kazalec-t;
+    else *kazalec = &t[*indeks];
+
+
+}
+
+void frekvenceCrk(char* niz, int** frekvence) {
+    // dopolnite ...
+}
+
+#ifndef test
+
+int main() {
+    // int seznam[] = {2,3, 1, 1, 1, 1, 2}; //sum = 11
+    // printf("sum= %d\n", vsota(seznam, &seznam[5]));
+
+
+    return 0;
+}
+
+#endif
