9d53aa58a114960d65962394e09d99608a9e6b11
kunejc <nejc.kunej@gmail.com>
2025-06-08 20:31:59 +0200

    vaje6

--- vaje/10/naloga.c
+++ vaje/10/naloga.c
@@ -6,55 +6,22 @@
 #include "naloga.h"
 
 int poisciStudenta(Student** studentje, int stStudentov, int vpisna) {
-    for (size_t i = 0; i < stStudentov; i++)
-    {
-        if (studentje[i]->vpisna == vpisna) return i;
-    }   
-    return -1;
+
+    
+        return 0;
 }
 
 int poisciPO(Student* student, char* predmet) {
-        for (size_t i = 0; i < student->stPO; i++)
-        {
-            if (!strcmp(student->po[i].predmet, predmet)) return i;
-                      
-        }   
-    return -1;
+
+    
+        return 0;
 }
 
 int dodaj(Student** studentje, int stStudentov, int vpisna, char* predmet, int ocena) {
-    int indexIskanega = poisciStudenta(studentje, stStudentov, vpisna);
-    if (indexIskanega == -1)
-    {
-        stStudentov++;
-        struct _Student* novStudent = malloc(sizeof(struct _Student));
-        novStudent->vpisna = vpisna;
-        novStudent->po = malloc(10*sizeof(struct _PO));
-        novStudent->po[0].ocena = ocena;
-        strcpy(novStudent->po[0].predmet, predmet);
-        novStudent->stPO = 1;
-
-        studentje[stStudentov-1] = novStudent;
-    }
-    else {
-        int indexPredmeta = poisciPO(studentje[indexIskanega], predmet);
-        if (indexPredmeta == -1)
-        {
-            int i = studentje[indexIskanega]->stPO;
-            struct _PO* novPredmet = malloc(sizeof(struct _PO));
-            strcpy(novPredmet->predmet, predmet);
-            novPredmet->ocena = ocena;
-            studentje[indexIskanega]->po[i] = *novPredmet;
-            studentje[indexIskanega]->stPO++;
-        }
-        else studentje[indexIskanega]->po[indexPredmeta].ocena = ocena;
-    
-    }
 
 
 
-
-    return stStudentov;
+    return 0;
 }
 
 #ifndef test
