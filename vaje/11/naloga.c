209ec089d61363a81a48d1df61e3ce3f7f03884b
kunejc <nejc.kunej@gmail.com>
2025-06-09 13:31:57 +0200

    vaje11

--- vaje/11/naloga.c
+++ vaje/11/naloga.c
@@ -25,21 +25,50 @@ int vsotaR(Vozlisce* zacetek) {
 }
 
 Vozlisce* vstaviUrejenoI(Vozlisce* zacetek, int element) {
+    Vozlisce *trenutno = zacetek;
+    // Vozlisce *pred = zacetek;
     
+    Vozlisce *novo = malloc(sizeof(Vozlisce));
+    novo->podatek = element;
+    novo->naslednje = NULL;
 
-
-
-
-
-
-
-
-
-    return NULL;
+    if(zacetek == NULL) return novo;
+    if (element <= trenutno->podatek)
+    {
+        novo->naslednje = trenutno;
+        return novo;
+    }
+    while (trenutno->naslednje != NULL)
+    {
+        if(trenutno->naslednje->podatek > element) {
+            novo->naslednje = trenutno->naslednje;
+            trenutno->naslednje = novo;
+            return zacetek;
+        } 
+        trenutno = trenutno->naslednje;
+    }
+    trenutno->naslednje = novo;
+        
+    return zacetek;
 }
 
 Vozlisce* vstaviUrejenoR(Vozlisce* zacetek, int element) {
-    // popravite / dopolnite ...
+    if(zacetek == NULL || zacetek->podatek > element) 
+    {
+        Vozlisce* novo = malloc(sizeof(Vozlisce));
+        novo->podatek = element;
+        novo->naslednje = zacetek;
+        return novo;
+    }
+    zacetek->naslednje = vstaviUrejenoR(zacetek->naslednje, element);
+    return zacetek;
+
+
+
+
+
+
+
     return NULL;
 }
 
